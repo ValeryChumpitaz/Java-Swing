39fb627cb99b7f31907cee7c85784ebfb97b86a7
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-04-10 09:59:04 -0500

    estructuraCarpetas

---
+++ GestorTareasE/src/main/java/vallegrande/edu/pe/gestortareas/App.java
@@ -0,0 +1,11 @@
+package vallegrande.edu.pe.gestortareas;
+
+
+import vallegrande.edu.pe.gestortareas.view.Main;
+
+public class App {
+    public static void main(String[] args) {
+        // Iniciar la interfaz de usuario
+        new Main();
+    }
+}
